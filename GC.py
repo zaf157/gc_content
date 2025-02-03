97ceb97f1f3786299d45ed2033c45354eb147fda
zaf157 <s22-2018926543@fish.du.ac.bd>
2025-02-03 15:27:09 +0600

    Add files via upload

---
+++ GC.py
@@ -0,0 +1,7 @@
+def gc_content(seq):
+    """GC content in a DNA/RNA sequence"""
+    return round((seq.count('C')+ seq.count('G')) / len(seq) * 100)
+
+DNA_GC = "ACGCTCGATCGTAGCGAGGAGCGATGACGATCGAGAGATAGAGCGCCCGTAGCGATCGAACGCT"
+
+print(gc_content(DNA_GC))
